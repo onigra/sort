5d06b23f1f2ea44c00571999168e8136a5d22967
onigra <3280467rec@gmail.com>
2015-02-04 18:36:17 +0900

    v1

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,7 +1,47 @@
 class Sort
   def self.asc(array)
+    res = []
+
+    while !array.empty? do
+      min = array.shift
+      flg = nil
+
+      array.each do |i|
+        if min > i
+          array << min
+          flg = 1
+          break
+        end
+      end
+
+      if flg.nil?
+        res << min
+      end
+    end
+
+    res
   end
 
   def self.desc(array)
+    res = []
+
+    while !array.empty? do
+      min = array.shift
+      flg = nil
+
+      array.each do |i|
+        if min < i
+          array << min
+          flg = 1
+          break
+        end
+      end
+
+      if flg.nil?
+        res << min
+      end
+    end
+
+    res
   end
 end
