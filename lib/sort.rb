741bd951badcb352989c0221dc83e0733ed51a13
onigra <3280467rec@gmail.com>
2015-02-04 20:47:01 +0900

    ruby syntax

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,33 +1,9 @@
 class Sort
   def self.asc(array)
-    array.size.times do |i|
-      min = i
-
-      i.upto (array.size - 1) do |idx|
-        if array[idx] < array[min]
-          min = idx
-        end
-      end
-
-      array[i], array[min] = array[min], array[i]
-    end
-
-    array
+    array.sort
   end
 
   def self.desc(array)
-    array.size.times do |i|
-      min = i
-
-      i.upto (array.size - 1) do |idx|
-        if array[idx] > array[min]
-          min = idx
-        end
-      end
-
-      array[i], array[min] = array[min], array[i]
-    end
-
-    array
+    array.sort.reverse
   end
 end
