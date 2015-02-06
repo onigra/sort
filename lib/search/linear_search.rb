b2d147b180767ef46e6d2c43ccc53559d8ba759b
onigra <3280467rec@gmail.com>
2015-02-06 20:28:57 +0900

    v1

--- lib/search/linear_search.rb
+++ lib/search/linear_search.rb
@@ -1,4 +1,18 @@
 class LinearSearch
   def self.run(list, search_number)
+    hit_index = nil
+
+    list.each_with_index do |item, i|
+      if item == search_number
+        hit_index = i
+        break
+      end
+    end
+
+    if hit_index.nil?
+      "#{search_number}は見つかりませんでした"
+    else
+      "#{search_number}が#{hit_index}番目に見つかりました"
+    end
   end
 end
